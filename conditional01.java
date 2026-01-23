a6c07105a4815b18c067d40374414f69ab8ce105
Juliana Lima <165281819+limajulianaa@users.noreply.github.com>
2026-01-23 20:22:13 -0300

    Add Conditional01 class with basic if-else statement

---
+++ conditional01.java
@@ -0,0 +1,12 @@
+public class Conditional01 {
+    public static void main(String[] args) {
+
+    byte b = 100;
+
+   if(b > 99) {
+        System.out.println("Verdadeiro");
+   }else {
+        System.out.println("Falso");
+   }
+  }
+}
