ed51023d33aa9bc5cdac7e004bd47124caf7131e
Juliana Lima <165281819+limajulianaa@users.noreply.github.com>
2026-01-23 19:28:53 -0300

    Implement Main class with variable declarations
    Added a Main class with variable declarations for different data types.

---
+++ main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args){
+        /*números inteiros = 1, 2 , 3 ...
+         byte => 8 bits => -128 a 127
+        short => 14 bits => -32.768 a 32.767
+        int => 8 bits => -2.147.483.648 a 2.147.483.647
+        long => 8 bits => -9.223.372.836.854.775.808 a 9.223.372.836.775.807
+        //números decimais = 1.0, 1.1, 1.2, 1.3 ....
+        float => 32 bits => precisao simples
+        double => bits => precisao dupla
+        string => repesentar palavras e frases usa ""
+        char => representar um unico caractere usa ''
+        boolean => true ou false
+            */
+        byte b = 100;
+        short s = 10000;
+        int i = 1000000;
+        long l = 100000L;
+        float f = 10.5f;
+        double d = 'A';
+        String str = "Juliana";
+        boolean bool = true;
+    }
+}
