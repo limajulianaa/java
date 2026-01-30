411f686e6c2a5fe8b0f0fee9052b91849a1e5717
Juliana Lima <165281819+limajulianaa@users.noreply.github.com>
2026-01-30 15:38:11 -0300

    Add PrintNumber class to print numbers

---
+++ PrintNumber.java
@@ -0,0 +1,7 @@
+public class PrintNumber {
+    /*unlike text, we don't put numbers inside double quotes*/
+    public static void main(String[] args){
+        System.out.println(80);
+        System.out.println(46);
+    }
+}
